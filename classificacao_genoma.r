2e6d6e01984097635d21a9854954f19821e4fb41
danielpcinalli <danielpcinalli@gmail.com>
2021-04-16 14:34:27 -0300

    Scripts R

---
+++ classificacao_genoma.r
@@ -0,0 +1,24 @@
+library(C50)
+library(caret)
+
+test <- read.csv('./ml-small-tratado/classificacao_genoma_test.csv')
+train <- read.csv('./ml-small-tratado/classificacao_genoma_train.csv')
+
+train$userId <- as.factor(train$userId)
+test$userId <- as.factor(test$userId)
+
+# train.X <-train[, c(1, 2, 4:1131)]
+train.X <-train[, c(1, 4:503)]
+train.y <- train[, 3]
+# test.X <- test[, c(1, 2, 4:1131)]
+test.X <- test[, c(1, 4:503)]
+test.y <- test[, 3]
+
+
+model <- C50::C5.0(train.X, train.y)
+summary(model)
+
+pred <- predict(model, test.X)
+confusionMatrix(data=pred, reference=test.y, positive = 'True')
+
+
